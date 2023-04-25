7e93ed2e9f92b530b801e4e7f2c688fabcce3818
Gemju Sherpa <gemju.s1@gmail.com>
2023-04-25 20:27:26 +1000

    project skeleton for foodfinder

---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<script setup>
+    import store from '../stores'
+    import { computed } from 'vue';
+
+    const meals = computed(()=> store.state.meals)
+</script>
+
+<template> 
+    <h1>{{meals[0]}}</h1>
+</template>
